f4359e1ada1f285073507373848712aeb0ce7d63
egg_kim <yellcat@naver.com>
2015-10-01 15:30:35 +0900

    member dto

--- SpringProgramming/src/main/java/com/mycompany/myapp/dto/Member.java
+++ SpringProgramming/src/main/java/com/mycompany/myapp/dto/Member.java
@@ -1,8 +1,70 @@
 package com.mycompany.myapp.dto;
 
 public class Member {
-	private String memberId;
-	private String memberPw;
-
-
+	private String id;
+	private String pw;
+	private String name;
+	private String nickName;
+	private String email;
+	private String tel;
+	private int win;
+	private int lose;
+	private String grage;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPw() {
+		return pw;
+	}
+	public void setPw(String pw) {
+		this.pw = pw;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getNickName() {
+		return nickName;
+	}
+	public void setNickName(String nickName) {
+		this.nickName = nickName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getTel() {
+		return tel;
+	}
+	public void setTel(String tel) {
+		this.tel = tel;
+	}
+	public int getWin() {
+		return win;
+	}
+	public void setWin(int win) {
+		this.win = win;
+	}
+	public int getLose() {
+		return lose;
+	}
+	public void setLose(int lose) {
+		this.lose = lose;
+	}
+	public String getGrage() {
+		return grage;
+	}
+	public void setGrage(String grage) {
+		this.grage = grage;
+	}
+	
+	
 }
