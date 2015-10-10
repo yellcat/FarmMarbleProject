95068ea54787f0ad28ebc9bdd1aa19d043170183
egg_kim <yellcat@naver.com>
2015-10-10 14:38:50 +0900

    주사위 값 나오기

--- SpringProgramming/src/main/java/com/mycompany/myapp/service/GameService.java
+++ SpringProgramming/src/main/java/com/mycompany/myapp/service/GameService.java
@@ -1,13 +1,14 @@
 package com.mycompany.myapp.service;
 
+import java.util.Random;
+
 import org.springframework.stereotype.Component;
 
 @Component
 public class GameService {
 
 	public int roll() {
-		//1~6까지의 수를 랜덤으로 리턴한다
-		return 0;
+		Random dice = new Random();
+		return dice.nextInt(6);
 	}
-
 }
