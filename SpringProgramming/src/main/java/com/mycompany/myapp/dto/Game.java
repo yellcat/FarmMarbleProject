8b4083e567cc681ee6b7bff0b9814dc5809ab98f
yellcat <yellcat@naver.com>
2015-10-13 18:29:47 +0900

    가격 정리 완료

--- SpringProgramming/src/main/java/com/mycompany/myapp/dto/Game.java
+++ SpringProgramming/src/main/java/com/mycompany/myapp/dto/Game.java
@@ -7,39 +7,51 @@ public class Game {
 	private List<Gamer> gamer;//게이머
 	private String rName;//방 이름
 	private String state;//방 상태(game 가능 여부)
-	private String character;
-	public String getCharacter() {
-		return character;
+	private static final Integer price [] = {
+			null, //0
+			700,  //1
+			800,  //2
+			null, //3
+			900,  //4
+			500,  //5
+			null, //6
+			500,  //7
+			1200,
+			null,
+			500,
+			1400,
+			null,
+			900,
+			1000,
+			null,
+			800,
+			1300,
+			null,
+			1500,
+			500,
+			null,
+			600,
+			600
+	};
+	public List<Gamer> getGamer() {
+		return gamer;
 	}
-	public void setCharacter(String character) {
-		this.character = character;
+	public void setGamer(List<Gamer> gamer) {
+		this.gamer = gamer;
 	}
-	public String getLocation() {
-		return location;
+	public String getrName() {
+		return rName;
 	}
-	public void setLocation(String location) {
-		this.location = location;
+	public void setrName(String rName) {
+		this.rName = rName;
 	}
-	public int getMoney() {
-		return money;
+	public String getState() {
+		return state;
 	}
-	public void setMoney(int money) {
-		this.money = money;
+	public void setState(String state) {
+		this.state = state;
 	}
-	public String getCity() {
-		return city;
+	public static Integer[] getPrice() {
+		return price;
 	}
-	public void setCity(String city) {
-		this.city = city;
-	}
-	public Map getTree() {
-		return tree;
-	}
-	public void setTree(Map tree) {
-		this.tree = tree;
-	}
-	private String location;
-	private int money;
-	private String city;
-	private Map tree;
 }
