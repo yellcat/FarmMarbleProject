6d52c03e913b748acb5ad4b39de966150f829f5b
dami815 <dmkim815@gmail.com>
2015-10-16 16:50:59 +0900

    game.js pay, upgradeSeed

--- SpringProgramming/src/main/webapp/resources/js/game.js
+++ SpringProgramming/src/main/webapp/resources/js/game.js
@@ -40,7 +40,70 @@ function buyDialgoue(data) {
 	      }
 	    });
 	    
-	    $("#"+nLoc).dialogue.html("구입하시겠습니까?");
+	    $("#"+ nLoc ).dialogue.html("구입하시겠습니까?");
 	  });	
 }
+
+function pay(data) {
+	var nLoc = data.nLoc;
+	
+	$(function() {
+	    $( "#" + nLoc ).dialog({
+	      autoOpen: true,
+	      height:200,
+	      width: 300,
+	     
+	      modal: true,
+	      buttons: {
+	        "확인": function() {
+	          $( this ).dialog( "close" );
+	        }	        
+	      },	      
+	      show: {
+	        effect: "blind",
+	        duration: 1000
+	      },
+	      hide: {
+	        effect: "explode",
+	        duration: 1000
+	      }
+	    });
+	    
+	    $("#"+ nLoc ).dialogue.html("이용료를 지불하십시오");
+	  });	
+}
+
+function upgradeSeed(data) {
+	var nLoc = data.nLoc;
+	
+	$(function() {
+	    $( "#" + nLoc ).dialog({
+	      autoOpen: true,
+	      height:200,
+	      width: 300,
+	     
+	      modal: true,
+	      buttons: {
+	        "네": function() {
+	          $( this ).dialog( "close" );
+	        },
+	        "아니오": function() {
+	          $( this ).dialog( "close" );
+	        }
+	      },	      
+	      show: {
+	        effect: "blind",
+	        duration: 1000
+	      },
+	      hide: {
+	        effect: "explode",
+	        duration: 1000
+	      }
+	    });
+	    
+	    $("#"+ nLoc ).dialogue.html("업그레이드하시겠습니까?");
+	  });	
+}
+
+
 
