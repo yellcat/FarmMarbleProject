5d046dbe4447857f241ba976a01a4c02a8961065
jgpg1029 <jgpg1029@nate.com>
2015-10-19 13:06:27 +0900

    sp

--- SpringProgramming/src/main/webapp/resources/js/Chat.js
+++ SpringProgramming/src/main/webapp/resources/js/Chat.js
@@ -1,7 +1,7 @@
 		var wschat=null;
 		chatconnect();
 		function chatconnect() {
-			wschat = new WebSocket("ws://"+window.location.host+"/myapp/chat-wschat");
+			wschat = new WebSocket("ws://"+window.location.host+"/SpringProgramming/chat-wschat");
  			
  			wschat.onopen = function(){
  				console.log("접속O");
